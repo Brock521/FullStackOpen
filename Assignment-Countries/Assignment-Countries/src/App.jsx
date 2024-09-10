0ae10219b130b35416b132db859f4f911d59840c
Brock Polnik <bpolnik1@gmail.com>
2024-09-09 23:33:35 -0500

    Exercise 2.19 Added button to manually select the country from list. Refactored clunky component rendering code.

--- Assignment-Countries/Assignment-Countries/src/App.jsx
+++ Assignment-Countries/Assignment-Countries/src/App.jsx
@@ -3,7 +3,12 @@ import './App.css'
 import axios from 'axios';
 import CountryList from './CountryList';
 
-/*Uses API from https://studies.cs.helsinki.fi/restcountries/ */
+/*Uses API from https://studies.cs.helsinki.fi/restcountries/ 
+https://fullstackopen.com/en/part2/adding_styles_to_react_app#exercises-2-16-2-17
+As outlined in the assignment there are some edge cases where some countries have simlar names so more filtering would be requried
+such as Sudan and South Sudan. No all functionality will work for these countries at the moment as it is not required.
+
+*/
 
 function App() {
   
@@ -16,14 +21,10 @@ function App() {
 
   },[]);
 
-  
-  
   return (
     <>     
       {
-        <CountryList countryData={countryData}/>
-        
-        
+        <CountryList countryData={countryData}/>        
       }
     </>
   )
--- Assignment-Countries/Assignment-Countries/src/CountryList.jsx
+++ Assignment-Countries/Assignment-Countries/src/CountryList.jsx
@@ -1,49 +1,59 @@
 import { useState } from "react";
-import Country from './Country'
+import Country from "./Country";
 
-export default function CountryList({countryData}){
-    const [activeCountries, setActiveCountries] = useState([]);
+export default function CountryList({ countryData }) {
+  const [activeCountries, setActiveCountries] = useState([]);
+  const [activeCountry, setActiveCountry] = useState(null);
 
-    function showFilteredCountries(event){
+  function showFilteredCountries(event) {
+    if (!countryData) {
+      console.log("Country data not available");
+      return;
+    }
 
-        if(!countryData){
-          console.log("Country data not available");
-          return;
-        }
-    
-        let countryListNames = countryData.filter((country)=>{
-          return country.name.common.toUpperCase().includes(event.target.value.toUpperCase());
-      });
-    
-      console.log(countryListNames);
-      setActiveCountries(countryListNames);
-    
-      } 
+    const filterTerm = event.target.value.toUpperCase();
+    const filteredCountries = countryData.filter((country) =>
+      country.name.common.toUpperCase().includes(filterTerm)
+    );
 
+    console.log(filteredCountries);
+    setActiveCountries(filteredCountries);
+    setActiveCountry(null);
+  }
 
-      return(
-        <>     
-       Find Country: <input onChange={showFilteredCountries}></input>
-        
-       {
-        activeCountries.length <= 10 ? 
+  function handleShowCountry(country) {
+    setActiveCountry(country);
+  }
 
-        activeCountries.length == 1 ? 
-        
-            <Country country={activeCountries[0]}/>
-        :
+  function renderCountryList() {
+    if (activeCountry) {
+      return <Country country={activeCountry} />;
+    }
 
-        activeCountries.map((info)=>{
-            return <p className={info.name.common}>{info.name.common}</p>
-          }) 
-          
-        :
-          
-        <p>Too many results returned</p>
+    if (activeCountries.length === 0) {
+      return null;
+    }
 
-        }
-        
-          </>
-      );
+    if (activeCountries.length === 1) {
+      return <Country country={activeCountries[0]} />;
+    }
 
+    if (activeCountries.length <= 10) {
+      return activeCountries.map((info) => (
+        <p key={info.name.common}>
+          {info.name.common}
+          <button onClick={() => handleShowCountry(info)}>show</button>
+        </p>
+      ));
+    }
+
+    return <p>Too many results returned</p>;
+  }
+
+  return (
+    <>
+      Find Country: <input onChange={showFilteredCountries} />
+      {renderCountryList()}
+    </>
+  );
 }
