e5e9348afd3d1393ec4d8d844227fa75816eb726
Brock Polnik <bpolnik1@gmail.com>
2024-10-16 23:03:19 -0500

    Exercise 6.13 Step 11: Added a timer so that the notification dissapears after 5 seconds

--- Assignment-Anecdotes/Assignment-Anecdotes/src/Notification.jsx
+++ Assignment-Anecdotes/Assignment-Anecdotes/src/Notification.jsx
@@ -1,17 +1,32 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { resetNotifMessage } from './Reducers/notificationReducer'; // Assume you have this action to clear the notification
 
 const Notification = () => {
-  const notification = useSelector(state => state.notification); 
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.notification);
+
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        dispatch(resetNotifMessage());
+      }, 5000);
+
+      // Cleanup the timer if the component unmounts or notification changes
+      return () => clearTimeout(timer);
+    }
+  }, [notification, dispatch]);
+
+  if (!notification) return null; // Don't render if there's no notification
 
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
-  }
-  return (
-    <div style={style}>
-      {notification}    </div>
-  )
-}
+    borderWidth: 1,
+    marginBottom: 10,
+  };
 
-export default Notification
+  return <div style={style}>{notification}</div>;
+};
+
+export default Notification;
